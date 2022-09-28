f028d5b74c0d86b0290f5a89a9ceb4bf14235bf0
Robin Fernandes <robin@soal.org>
2022-09-28 06:10:34 +0000

    Add option to include input palette in color correction window (either never, always or just on the 1st frame; change algorithm to base the cc target correction on the uncorrected outputs so we don't 'double-correct

--- loopback-cc-experiments.py
+++ loopback-cc-experiments.py
@@ -1,3 +1,10 @@
+# +--( Advanced color correction script for stable-diffusion-webui img2img loopback )--+
+# |                                                                                    |
+# | For more information and the latest version:                                       |
+# | https://github.com/rewbs/stable-diffusion-loopback-color-correction-script         |
+# |                                                                                    | 
+# +-----------------------------------------------------------------------------------+
+
 import numpy as np
 import logging
 from pprint import pformat
@@ -23,15 +30,15 @@ class Script(scripts.Script):
     def ui(self, is_img2img):
         loops = gr.Slider(minimum=1, maximum=500, step=1, label='Loops', value=4)
         denoising_strength_change_factor = gr.Slider(minimum=0.9, maximum=1.1, step=0.01, label='Denoising strength change factor', value=1)
-        cc_target_type = gr.Radio(label='Color correction target', choices=['window', 'input'], value='window')
+        cc_include_input = gr.Radio(label='Include input image in color target', choices=['never', 'first', 'always'], value='first')
         cc_window_size = gr.Slider(minimum=-1, maximum=50, step=1, label='Color correction window size', value=-1)
         cc_window_rate = gr.Slider(minimum=0.1, maximum=1, step=0.1, label='Color correction window slide rate', value=1)
-        cc_window_delay = gr.Slider(minimum=1, maximum=10, step=1, label='Color correction window delay', value=0)
+        cc_window_delay = gr.Slider(minimum=1, maximum=10, step=1, label='Color correction window lag', value=0)
         cc_interval = gr.Slider(minimum=1, maximum=50, step=1, label='Color correction interval', value=1)
 
-        return [loops, denoising_strength_change_factor, cc_target_type, cc_window_size, cc_window_rate, cc_window_delay, cc_interval]
+        return [loops, denoising_strength_change_factor, cc_include_input, cc_window_size, cc_window_rate, cc_window_delay, cc_interval]
 
-    def run(self, p, loops, denoising_strength_change_factor, cc_target_type, cc_window_size, cc_window_rate, cc_window_delay, cc_interval):
+    def run(self, p, loops, denoising_strength_change_factor, cc_include_input, cc_window_size, cc_window_rate, cc_window_delay, cc_interval):
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
         processing.fix_seed(p)
@@ -65,12 +72,13 @@ class Script(scripts.Script):
         opts.img2img_color_correction = False
         p.color_corrections = None
 
-        input_histogram = processing.setup_color_correction(p.init_images[0])
+        original_input_image_resized = images.resize_image(p.resize_mode, p.init_images[0], p.width, p.height)
 
         try:
 
             for n in range(batch_count):
                 history = []
+                uncorrected_images = []
 
                 for i in range(loops):
                     p.n_iter = 1
@@ -81,23 +89,8 @@ class Script(scripts.Script):
                     cc_window_start, cc_window_end  = self.compute_cc_target_window(loop_index, cc_window_delay, cc_window_size, cc_window_rate)
                     do_cc = loop_index%cc_interval==0
 
-                    p.extra_generation_params = {
-                        "Batch": n,
-                        "Loop:": loop_index,
-                        "Denoising strength change factor": denoising_strength_change_factor,
-                        "Color correction interval": cc_interval,
-                        "Color correction target type": cc_target_type,
-                        "Color correction window size": cc_window_size,
-                        "Color correction window slide rate": cc_window_rate,
-                        "Color correction on this image": do_cc,
-                        "Color correction window start on this image": cc_window_start,
-                        "Color correction window end on this image": cc_window_end
-                    }
-                    
                     state.job = f"Iteration {loop_index}/{loops}, batch {n + 1}/{batch_count}"
                     logging.info(f"it:{loop_index} - seed:{p.seed}; denoising_strength:{p.denoising_strength}; ")
-                    logging.info(pformat(p.extra_generation_params))
-                    logging.debug(pformat(p))
 
                     processed = processing.process_images(p)
 
@@ -106,22 +99,45 @@ class Script(scripts.Script):
                         initial_info = processed.info
 
                     init_img = processed.images[0]
-
-                    if cc_target_type == 'window':
-                        all_images = history.copy()
-                        all_images.append(processed.images[0])
-                        target_histogram = self.compute_cc_target(all_images, cc_window_start, cc_window_end)
+                    
+                    uncorrected_images.append(processed.images[0])
+                    if (cc_window_end>0):
+                        target_images = uncorrected_images[cc_window_start:cc_window_end]
                     else:
-                        target_histogram = input_histogram
+                        target_images = []
+
+                    if cc_include_input == 'first' and cc_window_start==0:
+                        target_images.append(original_input_image_resized)
+                        logging.info(f"Window start is 0 so including initital image in correction target (total target images: {len(target_images)})")
+                    elif cc_include_input == 'always': 
+                        target_images.append(original_input_image_resized)
+                        logging.info(f"Forcing initital image into correction target (total target images: {len(target_images)})")                            
+                    else:
+                        logging.info(f"Initital image NOT included in correction target (total target images: {len(target_images)})")
+                    target_histogram = self.compute_cc_target(target_images)
+
+
+                    p.extra_generation_params = {
+                        "Batch": n,
+                        "Loop": loop_index,
+                        "Denoising strength change factor": denoising_strength_change_factor,
+                        "Color correction interval": cc_interval,
+                        "Color correction input image use": cc_include_input,
+                        "Color correction on this image": do_cc,                        
+                        "Color correction window size (desired)": cc_window_size,
+                        "Color correction window size (effective)": len(target_images) if target_images is not None else 0,
+                        "Color correction window slide rate": cc_window_rate,
+                        "Color correction window lag": cc_window_delay,
+                        "Color correction window pos start on this image": cc_window_start,
+                        "Color correction window pos end on this image": cc_window_end
+                    }
+                    logging.info(pformat(p.extra_generation_params))
 
                     if do_cc:
                         if target_histogram is None:
-                            logging.info(f"Skipping color correction on loop {loop_index} (cc interval: {cc_interval}, target frames: {cc_window_start} to {cc_window_end})")                            
+                            logging.info(f"Skipping color correction on loop {loop_index} (cc interval: {cc_interval}, target frames: {cc_window_start} to {cc_window_end})")
                         else:
-                            if cc_target_type == 'window':
-                                logging.info(f"Applying color correction on loop {loop_index} (cc interval: {cc_interval}, target frames: {cc_window_start} to {cc_window_end})")
-                            else:
-                                logging.info(f"Applying color correction on loop {loop_index} (cc interval: {cc_interval}, using input image)")
+                            logging.info(f"Applying color correction on loop {loop_index} (cc interval: {cc_interval}; target frames: {cc_window_start} to {cc_window_end}; effective window size: {len(target_images)})")
                             init_img = processing.apply_color_correction(target_histogram, init_img)
                             #images.save_image(init_img, p.outpath_samples, "", p.seed, p.prompt, opts.samples_format, info=None, p=p, suffix="-after-color-correction")
                     else:
@@ -158,19 +174,15 @@ class Script(scripts.Script):
             cc_window_start = max(0, cc_window_end-window_size)
         return cc_window_start, cc_window_end
 
+    def compute_cc_target(self, target_images):
+        if target_images is None or len(target_images)==0:
+            return None
 
-    def compute_cc_target(self, all_images, cc_window_start, cc_window_end):
-        target_histogram = None
-        
-        if (cc_window_start<cc_window_end and cc_window_start>=0):
-            target_images = all_images[cc_window_start:cc_window_end]
-            logging.debug(f"target_images length: {len(target_images)}")
-            
-            target_histogram = (cv2.cvtColor(np.asarray(target_images[0].copy()), cv2.COLOR_RGB2LAB)*0).astype('float64')
-            for img in target_images:
-                target_histogram_component = cv2.cvtColor(np.asarray(img.copy()), cv2.COLOR_RGB2LAB).astype('float64')
-                target_histogram += (target_histogram_component/len(target_images)).astype('float64')
-                        
-            target_histogram=target_histogram.astype('uint8')
+        target_histogram = (cv2.cvtColor(np.asarray(target_images[0].copy()), cv2.COLOR_RGB2LAB)*0).astype('float64')
+        for img in target_images:
+            target_histogram_component = cv2.cvtColor(np.asarray(img.copy()), cv2.COLOR_RGB2LAB).astype('float64')
+            target_histogram += (target_histogram_component/len(target_images)).astype('float64')
+                    
+        target_histogram=target_histogram.astype('uint8')
         
         return target_histogram
